9509c6e56ddb125eedc09f0cee74c0dff1174585
Houu <shadowbellcbvn@gmail.com>
2020-10-22 09:12:16 +0700

    Update code

--- data.py
+++ data.py
@@ -1,44 +1,48 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-# GET ARTICLE LINKS FROM 5 PAGES OF 24H.COM.VN
-linksarr = []
-articlearr = []
-total = []
-count = 0
 set_url = set()
-for i in range(1, 6):
-    print()
-    url = "https://www.24h.com.vn/kham-pha-cong-nghe-c675.html?vpage=" + str(i)
-    html_page = urllib.request.urlopen(url)
-    soupp = BeautifulSoup(html_page, "html.parser")
-    links = soupp.find_all('span', attrs={"class": "nwsTit postname"})
-    # print()
-    for link in links:
-        set_url.add(link.find('a', href=True)['href'])
+categories = ["Cong Nghe", "Du Lich", "Am Thuc", "Quan Su", "Thoi Trang"]
+categoriesurl = ["",
+                "https://www.24h.com.vn/kham-pha-cong-nghe-c675.html?vpage=",
+                "https://www.24h.com.vn/du-lich-24h-c76.html?vpage=",
+                "https://www.24h.com.vn/am-thuc-c460.html?vpage=",
+                "https://www.24h.com.vn/quan-su-c705.html?vpage=",
+                "https://www.24h.com.vn/the-gioi-thoi-trang-c672.html?vpage="]
 
-linksarr = list(set_url)
+count = 0
 
-for link in linksarr:
-    page = urllib.request.urlopen(link)
-    soup = BeautifulSoup(page, 'html.parser')
-    article = soup.find_all("p", text=True)
-    for art in article:
-        art = art.text.replace('\xa0', ' ').replace(
+for n in range(1, 6):
+    for i in range(1, 6):
+        # LINK FROM ARRAY
+        urll = categoriesurl[n] + str(i)
+        html_page = urllib.request.urlopen(urll)
+        soupp = BeautifulSoup(html_page, "html.parser")
+        links = soupp.find_all('span', attrs={"class": "nwsTit postname"})
+        for link in links:
+            # print(link.find('a', href=True)['href'])
+            set_url.add(link.find('a', href=True)['href'])
+        linksarr = list(set_url)
+        print(len(linksarr))
+        for link in linksarr:
+            articlearr = []
+            page = urllib.request.urlopen(link)
+            soup = BeautifulSoup(page, 'html.parser')
+            article = soup.find_all("p", text=True)
+            for art in article:
+                art = art.text.replace('\xa0', ' ').replace(
                     'Cơ quan chủ quản: Công ty Cổ phần Quảng cáo Trực tuyến 24H Trụ sở: Tầng 12, Tòa nhà Geleximco, '
                     '36 Hoàng Cầu, Phường Ô Chợ Dừa, Quận Đống Đa, TP Hà Nội. Tel: (84-24) 73 00 24 24 hoặc (84-24) '
                     '3512 1806 - Fax: (84-24) 3512 1804. Chi nhánh: Tầng 7, Tòa nhà Việt Úc, 402 Nguyễn Thị Minh Khai, '
                     'Phường 5, Quận 3, TP. Hồ Chí Minh. Tel: (84-28) 7300 2424 / Giấy phép số 332/GP – TT ĐT ngày cấp '
                     '22/01/2018 SỞ THÔNG TIN VÀ TRUYỀN THÔNG HÀ NỘI. Chịu trách nhiệm xuất bản: Phan Minh Tâm. '
                     'HOTLINE: 0965 08 24 24', ' ')
-        articlearr.append(art)
-        articlearr = [''.join(articlearr)]
-        total.append(articlearr)
-
-#             count = count + 1
-#             print(str(count) + ' Loading ...')
-# print(len(total))
-#     text_file = open("Cong Nghe/article_" + str(i) + ".txt", "w", encoding='utf-8')
-#     text_file.write(str(articlearr))
-
-print(len(total))
+                articlearr.append(art)              # Douma cho nay dung roi ban oi
+            articlearr = [''.join(articlearr)]
+            count = count + 1
+            print('Loading ' + str(count) + '...')
+            # CATEGORY FOLDER
+            text_file = open(categories[n] + "/article_" + str(i) + str(count) + ".txt", "w", encoding="utf-8")
+            text_file.write(str(articlearr))
+            del articlearr
+        del linksarr
--- vi.py
+++ vi.py
@@ -1,22 +1,12 @@
-# -*- coding: utf-8 -*-
-import vi_spacy_model
-import time
-from pyvi import ViTokenizer, ViPosTagger, ViUtils, ViDiac
-docs = (u"Giờ tôi lại lang thang. Tình yêu thì miên man. Ngày xanh cùng mây tung tăng tựa mình bên phím đàn. Nhìn em mình ngơ ngác. Lòng anh chợt hơi khác. Tình yêu này đến đúng lúc thấy ánh sáng vụt qua. Nụ cười tỏa hương nắng. Bình minh và mây trắng. Hình như đều kêu tôi ôi thôi tình yêu đến rồi! Chẳng ai phải thắc. Còn tôi thì đã chắc. Nàng ơi nàng hãy đến chiếm lấy tâm hồn tôi. Mỉm cười lòng chợt bâng khuâng tôi chẳng biết mơ hay thật. Đợi chờ dù ngày hay đêm anh chỉ cần nghĩ cũng thấy vui.")
+from bs4 import BeautifulSoup
+import urllib.request
 
-start = time.time()
-nlp = vi_spacy_model.load()
-doc = nlp(docs)
-arr = []
-for token in doc:
-    arr.append(token.text)
-print("Đây là SpaCcy óc chó")
-print(arr)
-
-from underthesea import sent_tokenize, word_tokenize, pos_tag, sentiment
-print("Đây là Dưới đáy biển")
-print(word_tokenize(docs))
-end = time.time() - start
-print(end)
-
+articlearr = []
+page = urllib.request.urlopen("https://www.24h.com.vn/the-gioi-thoi-trang/nhung-doi-giay-balenciaga-chua-bao-gio-"
+                              "het-hot-c672a1169533.html")
+soup = BeautifulSoup(page, 'html.parser')
+article = soup.find_all("p", text=True)
+print(article)
+# for art in article:
+#     art = art.text
 
